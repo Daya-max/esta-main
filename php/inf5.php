f1035e4782cdc40b4310308223d756c3333fc165
Daya-max <dayana101102@gmail.com>
2022-11-27 15:53:40 -0600

    Ya casi está listo :D

--- php/inf5.php
+++ php/inf5.php
@@ -10,17 +10,24 @@
     <link rel="stylesheet" href="../css/bitacora.css">
     <link rel="stylesheet" href="../css/styles.css">
 
-
     <script src="https://kit.fontawesome.com/92927bfbd1.js" crossorigin="anonymous"></script>
 
 </head>
-<?php require('style_log.php') ?>
+    <?php require('style_log.php')?>
 </body>
-<main>
-    <?php require('inf5.php')?>
 
+<main>
+    <?php require('index.php') ?>
+    <?php require('bdd.php') ?>
+    <?php require('addEvent.php') ?>
+    <?php require('editEventDate.php') ?>
+    <?php require('editEventTitle.php') ?>
 
 </main>
-<script src="../js/scripts.js"></script>
+<script src="../js/boton.js"></script>
+<script src="../js/menu.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+    crossorigin="anonymous"></script>
 <script src="../js/menu.js"></script>
 </body>
