ec1ecebc0d551f901aa1eec10cf9059ef0d1e2ab
Daya-max <dayana101102@gmail.com>
2022-11-27 23:09:49 -0600

    estamos a casi nadaaaa

--- php/inf3.php
+++ php/inf3.php
@@ -17,11 +17,54 @@
     <br>
     <br>
     <center>
-        <video controls width="600">
-            <source src="../video/4-7-8.mp4">
-            </source>
-
-        </video>
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+                    aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                    aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                    aria-label="Slide 3"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"
+                    aria-label="Slide 4"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active center">
+                    <video controls width="600">
+                        <source src="../video/4-7-8.mp4">
+                        </source>
+                    </video>
+                </div>
+                <div class="carousel-item">
+                    <video controls width="600">
+                        <source src="../video/4-7-8.mp4">
+                        </source>
+                    </video>
+                </div>
+                <div class="carousel-item">
+                    <video controls width="600">
+                        <source src="../video/4-7-8.mp4">
+                        </source>
+                    </video>
+                </div>
+                <div class="carousel-item">
+                    <video controls width="600">
+                        <source src="../video/4-7-8.mp4">
+                        </source>
+                    </video>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
     </center>
     <br>
     <br>
@@ -29,20 +72,74 @@
     <br>
     <br>
     <div class="container text-center">
-        <div class="row row-cols-4">
+        <div class="row row-cols-2">
             <div class="col">
-                <h5 class="card-title">formas de meditar</h5>
+                <h5 class="card-title">ways to meditate</h5>
+                <p>Meditation is simpler (and harder) than most people think. Read these steps, make sure you’re
+                    somewhere where you can relax into this process, set a timer, and give it a shot:
+
+                    1) Take a seat
+                    Find place to sit that feels calm and quiet to you.
+                    <br>
+
+                    2) Set a time limit
+                    If you’re just beginning, it can help to choose a short time, such as five or 10 minutes.
+                    <br>
+
+                    3) Notice your body
+                    You can sit in a chair with your feet on the floor, you can sit loosely cross-legged, you can
+                    kneel—all are fine. Just make sure you are stable and in a position you can stay in for a while.
+                    <br>
+
+                    4) Feel your breath
+                    Follow the sensation of your breath as it goes in and as it goes out.
+
+                    <br>
+
+                    5) Notice when your mind has wandered
+                    Inevitably, your attention will leave the breath and wander to other places. When you get around to
+                    noticing that your mind has wandered—in a few seconds, a minute, five minutes—simply return your
+                    attention to the breath.
+
+                    <br>
+                    6) Be kind to your wandering mind
+                    Don’t judge yourself or obsess over the content of the thoughts you find yourself lost in. Just come
+                    back.
+
+                    <br>
+                    7) Close with kindness
+                    When you’re ready, gently lift your gaze (if your eyes are closed, open them). Take a moment and
+                    notice any sounds in the environment. Notice how your body feels right now. Notice your thoughts and
+                    emotions.
+
+                    <br>
+
+                    That’s it! That’s the practice. You focus your attention, your mind wanders, you bring it back, and
+                    you try to do it as kindly as possible (as many times as you need to).
+                </p>
             </div>
             <div class="col">
-                <h5 class="card-title">tipos de aromatizantes para meditar</h5>
+                <h5 class="card-title">types of scents for meditation</h5>
+                <br>
+                <br>
+                <a title="aromas"
+                    href="https://articulo.mercadolibre.com.mx/MLM-1383804702-kit-12-esencias-aromaticas-de-16ml-difusor-vela-o-jabon-_JM?matt_tool=60989938&matt_word=&matt_source=google&matt_campaign_id=15701239400&matt_ad_group_id=141677971438&matt_match_type=&matt_network=g&matt_device=c&matt_creative=619488294679&matt_keyword=&matt_ad_position=&matt_ad_type=pla&matt_merchant_id=114353509&matt_product_id=MLM1383804702&matt_product_partition_id=1731524912294&matt_target_id=pla-1731524912294&gclid=Cj0KCQiAsoycBhC6ARIsAPPbeLuSV3NrPEukPCr-gz2Kc4lWIGrx58HjqPNo1205b9KPrUzONFaqvGMaApV5EALw_wcB"><img
+                        src="../images/aromas.png" class="rounded mx-auto d-block img-thumbnail" width="150"
+                        class="my-2" alt="..." /></a>
+                <br>
+                <br>
+                <br>
+
+                <a title="aromas"
+                    href="https://articulo.mercadolibre.com.mx/MLM-961635365-12-esencias-aromaticas-para-difusor-vela-o-humidificador-_JM#reco_item_pos=1&reco_backend=machinalis-seller-items-pdp&reco_backend_type=low_level&reco_client=vip-seller_items-above&reco_id=5ca56e84-636b-4ca6-8cab-6394db507b47"><img
+                        src="../images/o2.png" class="rounded mx-auto d-block img-thumbnail" width="150" class="my-2"
+                        alt="..." /></a>
+                <br>
+                <br>
             </div>
-            <div class="col">
-                <h5 class="card-title">privado</h5>
-            </div>
-            <div class="col">
-                <h5 class="card-title">privado</h5>
-            </div>
+
         </div>
+    </div>
     </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
