57976bfd99310c5d63f50c31e1fc835580fefdf0
Daya-max <dayana101102@gmail.com>
2022-11-29 16:49:13 -0600

    AI AIULAAA

--- php/mapaches.php
+++ php/mapaches.php
@@ -8,6 +8,7 @@ if (isset($_POST["feli-btn"])) {
     $emocionN = 'feliz';
     $branche = $_POST["feli-select"];
     $desc = $_POST["text_feli"];
+    $color = '#FFD700';
 
     $sql = "INSERT INTO log_feels(id,id_emocion,nombre_emocion,id_emc_desagrado,id_emc_felicidad,	
     id_emc_ira,id_emc_miedo,id_emc_sorpresa,id_emc_tristeza,descripcion,created_at)
@@ -15,9 +16,11 @@ if (isset($_POST["feli-btn"])) {
 
     $resultado = mysqli_query($conexion,$sql);
 
-
-
     
+    // $sql1 = "INSERT INTO events(title,color,start) VALUES('$branche',$color, now() )";
+    // $resultado = mysqli_query($conexion,$sql1);
+
+
     if($resultado){
         echo "si jala";
     }
